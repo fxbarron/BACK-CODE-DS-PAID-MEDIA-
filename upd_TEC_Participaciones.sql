37e053ee3a299e6dcd7ece530eb7dbe6ab8abdba
fxbarron <150094970+fxbarron@users.noreply.github.com>
2025-01-24 11:42:43 -0600

    Update upd_TEC_Participaciones.sql

--- upd_TEC_Participaciones.sql
+++ upd_TEC_Participaciones.sql
@@ -1,6 +1,9 @@
 # Despende de `primal-sunup-357522.TEC_LAYOUT_OBJETIVOS.TEC_Stg_P2C_Part`
 
-INSERT INTO `primal-sunup-357522.TEC_LAYOUT_OBJETIVOS.TEC_Participaciones`
+-- TRUNCATE TABLE `primal-sunup-357522.TEC_LAYOUT_OBJETIVOS.TEC_Participaciones`
+
+-- INSERT INTO `primal-sunup-357522.TEC_LAYOUT_OBJETIVOS.TEC_Participaciones`
+
 SELECT FECHA_CONVERSION as Fecha,Funnel,CAMPAIGN_FUNNEL AS Campaign,Canal,Medio,count(distinct P2C_ID_DL) AS Paticipaciones FROM
 `primal-sunup-357522.TEC_LAYOUT_OBJETIVOS.TEC_Stg_P2C_Part` where FUNNEL <> 'NA'
 GROUP BY 1,2,3,4,5 ORDER BY FECHA_CONVERSION ASC;
